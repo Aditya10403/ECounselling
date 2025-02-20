69075c58aff1d44d98424d3f997fb52284db6d31
Aditya10403 <adityashukla@gmail.com>
2025-02-20 08:46:18 +0530

    feat: update (filter, util) for Jwt modules.

--- src/main/java/com/ECounselling/filter/JwtFilter.java
+++ src/main/java/com/ECounselling/filter/JwtFilter.java
@@ -2,6 +2,7 @@ package com.ECounselling.filter;
 
 import com.ECounselling.util.JwtUtil;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.MalformedKeyException;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
@@ -13,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -32,27 +34,28 @@ public class JwtFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String authHeader = request.getHeader("Authorization");
+        String authorizationHeader = request.getHeader("Authorization");
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        String token = authHeader.substring(7);
+        String token = authorizationHeader.substring(7);
         String mailId;
 
         try {
             mailId = jwtUtil.extractMailId(token);
             if (mailId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(mailId);
-                if (jwtUtil.isTokenValid(token, userDetails)) {
+                if (jwtUtil.validateToken(token, userDetails)) {
                     UsernamePasswordAuthenticationToken authenticationToken =
                             new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                 }
             }
-        } catch (ExpiredJwtException ex) {
+        } catch (ExpiredJwtException | MalformedJwtException ex) {
             request.setAttribute("expired", "true");
         } catch (SignatureException | MalformedKeyException ex) {
             request.setAttribute("invalid", "true");
--- src/main/java/com/ECounselling/util/JwtUtil.java
+++ src/main/java/com/ECounselling/util/JwtUtil.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Getter
@@ -18,17 +20,20 @@ public class JwtUtil {
     @Value("${jwt.secret-key}")
     private String SECRET_KEY;
 
-    private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-    }
-
     public String generateToken(String mailId) {
+        Map<String, Object> claims = new HashMap<>();
+
         return Jwts.builder()
+                .claims(claims)
                 .subject(mailId)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 2)) // 6hrs - 1000 * 60 * 60 * 6
                 .signWith(getSigningKey())
                 .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 
     public String extractMailId(String token) {
@@ -48,12 +53,16 @@ public class JwtUtil {
                 .getPayload();
     }
 
-    public boolean isTokenValid(String token, UserDetails userDetails) {
+    public boolean validateToken(String token, UserDetails userDetails) {
         String mailId = extractMailId(token);
         return (mailId.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     private boolean isTokenExpired(String token) {
-        return extractClaim(token, Claims::getExpiration).before(new Date());
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
